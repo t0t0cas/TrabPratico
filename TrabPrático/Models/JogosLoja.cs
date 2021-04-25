b9cb6a3370ef0cc4a4b20269d290eb16ed268c6b
António Fialho <aluno21895@ipt.pt>
2021-04-25 14:22:30 +0100

    Pequena atualização

--- TrabPrático/Models/JogosLoja.cs
+++ TrabPrático/Models/JogosLoja.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,25 @@ namespace TrabPrático.Models
 {
     public class JogosLoja
     {
+        /// <summary>
+        /// PK para a tabela de relacionamento entre Cães e Criadores
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
 
+        /***********************************/
+        /// <summary>
+        /// FK para a Loja 
+        /// </summary>
+        [ForeignKey(nameof(Loja)]
+        public int LojaFK { get; set; }
+        public Loja Loja { get; set; }
+
+        /// <summary>
+        /// FK para os Jogos
+        /// </summary>
+        [ForeignKey(nameof(Jogos)]
+        public int JogosFK { get; set; }
+        public Jogos Jogos { get; set; }
     }
 }
--- TrabPrático/Models/Utilizador.cs
+++ TrabPrático/Models/Utilizador.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace TrabPrático.Models
         /// <summary>
         /// Id do utilizador 
         /// </summary>
+        [Key]
         public int IdUtilizador { get; set; }
 
         /// <summary>
