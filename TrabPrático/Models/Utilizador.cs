307f1c212e31223d9da0896ef10db16209010fd0
António Fialho <aluno21895@ipt.pt>
2021-04-27 14:32:11 +0100

    Pequena alteração 2.0

--- TrabPrático/Models/Utilizador.cs
+++ TrabPrático/Models/Utilizador.cs
@@ -22,21 +22,21 @@ namespace TrabPrático.Models
         /// <summary>
         /// Nome do utilizador
         /// </summary>
-        [Required(ErrorMessage = "O Nome é de preenchimento obrigatório")]
+        [Required(ErrorMessage = "O nome é de preenchimento obrigatório")]
         [StringLength(60, ErrorMessage = "O {0} não pode ter mais de {1} caracteres.")]
         public string Nome { get; set; }
 
         /// <summary>
         /// Email do Utilizador
         /// </summary>
-        [Required(ErrorMessage = "O Nome é de preenchimento obrigatório")]
+        [Required(ErrorMessage = "O email é de preenchimento obrigatório")]
         [StringLength(50, ErrorMessage = "O {0} não pode ter mais de {1} caracteres.")]
         public string Email { get; set; }
 
         /// <summary>
         /// Password do utilizador
         /// </summary>
-        [Required(ErrorMessage = "O Nome é de preenchimento obrigatório")]
+        [Required(ErrorMessage = "A password é de preenchimento obrigatório")]
         [StringLength(12, ErrorMessage = "O {0} não pode ter mais de {1} caracteres.")]
         public string Pass { get; set; }
 
